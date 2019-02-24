2e414b745ab29d0b2881af534b0562a89c0b0a44
Onlyzhang16 <zhangssmail@gamil.com>
2019-02-24 23:40:17 +0800

    LeetCode-无重复字符的最长子串

---
+++ algorithm/src/LengthOfLongestSubstring.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LengthOfLongestSubstring {
+    public static int lengthOfSubstring(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        int now = 0;
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                now = Math.max(map.get(s.charAt(i)) + 1,now);
+                if (maxLength < (i - now + 1)) {
+                    maxLength = i - now + 1;
+                }
+            } else {
+                if (maxLength < (i - now + 1)) {
+                    maxLength = i - now + 1;
+                }
+            }
+            map.put(s.charAt(i),i);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String str = "abcdrr";
+        String str1 = "aaaaaaaaaa";
+        System.out.println(LengthOfLongestSubstring.lengthOfSubstring(str));
+        System.out.println(LengthOfLongestSubstring.lengthOfSubstring(str1));
+    }
+}
