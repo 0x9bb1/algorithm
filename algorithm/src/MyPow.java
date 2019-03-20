b703c988d6c3db51f0b8d47015e9b546338cce8b
Onlyzhang16 <zhangssmail@gamil.com>
2019-03-21 00:50:21 +0800

    LeetCode-Pow(x,n)

---
+++ algorithm/src/MyPow.java
@@ -0,0 +1,35 @@
+public class MyPow {
+
+    public double myPow(double x, int n) {
+        if (n < 0) {
+            return 1 / pow(x,n);
+        } else {
+            return pow(x,n);
+        }
+    }
+
+    private double pow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        if (n == 1) {
+            return x;
+        }
+
+        double val = pow(x, n / 2);
+
+        if (n % 2 == 0) {
+            return val * val;
+        } else {
+            return val * val * x;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyPow myPow = new MyPow();
+        System.out.println(
+                myPow.myPow(3,4)
+        );
+    }
+}
