e61c6916fb7032c6824965746e8da5deb1e3f75e
zhangnix <zhangssmail@gmail.com>
2019-09-08 15:29:46 +0800

    reverse linkedList

---
+++ algorithm/src/ReverseList.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+
+public class ReverseList {
+
+    private static Node1 reverse(Node1 curr) {
+        Node1 prev = null;
+        Node1 next;
+
+        while (null != curr) {
+            next = curr.getNext();
+            curr.setNext(prev);
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Node1 last = new Node1(2,null);
+        Node1 first = new Node1(1,last);
+        System.out.println(first);
+
+        reverse(first);
+        System.out.println("----reverse----");
+        System.out.println(first);
+    }
+
+}
+
+class Node1 {
+    private Integer value;
+
+    private Node1 next;
+
+    public Node1(Integer value, Node1 next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Node1 getNext() {
+        return next;
+    }
+
+    public void setNext(Node1 next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node1{" +
+                "value=" + value +
+                ", next=" + next +
+                '}';
+    }
+}
