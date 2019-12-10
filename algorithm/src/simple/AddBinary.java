745ff8a76c94ca85d3b3258713ed0aa746b25151
zhangxin <zhangxin@odianyun.com>
2019-12-10 22:08:56 +0800

    LeetCode-67. 二进制求和

---
+++ algorithm/src/simple/AddBinary.java
@@ -0,0 +1,23 @@
+package simple;
+
+public class AddBinary {
+
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int ca = 0;
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+            int sum = ca;
+            sum += i >= 0 ? a.charAt(i) - '0' : 0;
+            sum += j >= 0 ? b.charAt(j) - '0' : 0;
+            sb.append(sum % 2);
+            ca = sum / 2;
+        }
+        sb.append(ca == 1 ? ca : "");
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        AddBinary addBinary = new AddBinary();
+        System.out.println(addBinary.addBinary("1010","1011"));
+    }
+}
