57df0a30b2b577675b15ae09d9252c7e19ca64cd
zhangnix <xin_zhang_naive@outlook.com>
2020-07-01 15:57:25 +0800

    leetcode-100.相同的树

---
+++ algorithm/src/main/java/simple/IsSameTree.java
@@ -0,0 +1,51 @@
+package main.java.simple;
+
+import javax.swing.tree.TreeNode;
+import java.util.Objects;
+import java.util.Stack;
+
+/**
+ * 100. 相同的树
+ *
+ * @author zhangnix
+ * @date created 2020/07/01
+ */
+public class IsSameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (Objects.isNull(p) && Objects.isNull(q)) {
+            return true;
+        }
+
+        if (Objects.isNull(p) || Objects.isNull(q)) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        System.out.println(new IsSameTree().isSameTree(p, q));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
