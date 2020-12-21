d7680f15c418120da770aa543d29f426291f4f00
zhangnix <zhangssmail@gmail.com>
2020-12-21 21:36:46 +0800

    leetCode-56. 合并区间

---
+++ algorithm/src/main/java/medium/Merge.java
@@ -0,0 +1,43 @@
+package main.java.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 56. 合并区间
+ */
+public class Merge {
+
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : intervals) {
+            int l = interval[0];
+            int r = interval[1];
+
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < l) {
+                merged.add(new int[]{l, r});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], r);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    public static void main(String[] args) {
+        Merge merge = new Merge();
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] intervals2 = {{1, 4}, {4, 5}};
+
+        System.out.println(Arrays.deepToString(merge.merge(intervals)));
+        System.out.println(Arrays.deepToString(merge.merge(intervals2)));
+    }
+}
